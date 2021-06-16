6efb1d7d78476268c098017905bf4f86ac82b2c2
Jan <janv83@gmail.com>
2021-06-17 01:54:31 +0200

    added window again

--- src/SabroshiLoader.js
+++ src/SabroshiLoader.js
@@ -1,44 +1,40 @@
 import React, {useEffect, useState} from 'react';
 
 function SabroshiLoader(props) {
-    let root = props.windowInstance;
-    let document2 = props.documentInstance;
     let [runLoaded, setRunLoaded] = useState(false);
     let sabroshiAvatar = props.sabroshiAvatar;//useParams();
-    console.log("sl loaded");
-    if(root.localStorage && typeof document2 !== "undefined")
-    {
-        console.log("has instances");
+    console.log("sl loaded " + sabroshiAvatar);
+ 
     useEffect(() => {
    
    
     
-    if(sabroshiAvatar || root.localStorage.sabroshiAvatar)  // user has requested that his avatar should be displayed in your app
+    if(sabroshiAvatar || window.localStorage.sabroshiAvatar)  // user has requested that his avatar should be displayed in your app
     {
         if(sabroshiAvatar && sabroshiAvatar !== "disconnect")  
         {
-            root.localStorage.sabroshiAvatar = sabroshiAvatar;  //saving the avatarlocation of the user in localstorage
+            window.localStorage.sabroshiAvatar = sabroshiAvatar;  //saving the avatarlocation of the user in localstorage
         
                     console.log("about to add scripts");  
 
                 //loading of the webversion of the bsv and run-sdk library
-        const script =  document2.createElement('script');
+        const script =  document.createElement('script');
         script.type="text/javascript";
         script.src = "https://bico.media/2ae5137168cb00d204becebe83bc54af863392bcbc3f0da0340363aa263c6ed1.js";
         script.async = "true";
         script.addEventListener('load',()=>{ console.log("bsvloaded");
         
-        const script2 = document2.createElement('script');
+        const script2 = document.createElement('script');
         script2.type="text/javascript";
         script2.src = "https://bico.media/7429a95bf9f7d1b9bebd05031f7bd32aa2f00e97e01271ec182294f1cdb65642.js";
         script2.async = "true";
         script2.addEventListener('load',()=>{console.log("runloaded"); initRun(props.width, props.height, true).then(()=>{setRunLoaded(true)}); });
 
-        document2.body.appendChild(script2);
+        document.body.appendChild(script2);
 
        });
 
-        document2.body.appendChild(script);
+        document.body.appendChild(script);
 
         
         }
@@ -46,9 +42,9 @@ function SabroshiLoader(props) {
         {
                 if(sabroshiAvatar === "disconnect")
                 {
-                    root.localStorage.removeItem("sabroshiAvatar");
-                    root.localStorage.removeItem("sabroshiDataURL");
-                    root.localStorage.sabroshiDisconnect = true;  //setting disconnect so Apps can  handle cleanup
+                    window.localStorage.removeItem("sabroshiAvatar");
+                    window.localStorage.removeItem("sabroshiDataURL");
+                    window.localStorage.sabroshiDisconnect = true;  //setting disconnect so Apps can  handle cleanup
                 }
                 
         }
@@ -59,7 +55,7 @@ function SabroshiLoader(props) {
     }
 
 
-      , []); }
+      , []); 
     
     return (<div id="sabroshiContainer"></div>);
 }
@@ -73,19 +69,19 @@ async function initRun(width, height, client)
     run.cache = cacheRun;
     run.client = client;
     try{
-    let ava = await loadAvatar(run, root.localStorage.sabroshiAvatar);
+    let ava = await loadAvatar(run, window.localStorage.sabroshiAvatar);
     if(ava && ava.owner === ava.activeSabroshi.owner)
     {
     let imgTag = new Image();
     imgTag.src="data:image/png;base64," + ava.activeSabroshi.metadata.image.base64Data;
-    root.localStorage.sabroshiDataURL = imgTag.src; //sets the image data to be used elsewhere in localstorage
+    window.localStorage.sabroshiDataURL = imgTag.src; //sets the image data to be used elsewhere in localstorage
     imgTag.className ="sabroshiPicture";
     if(widht && height)
     {
     imgTag.width = width;
     imgTag.heigh = height;
     }
-    document2.getElementById("sabroshiContainer").append(imgTag);
+    document.getElementById("sabroshiContainer").append(imgTag);
     }
     else
     {
@@ -94,6 +90,7 @@ async function initRun(width, height, client)
     }
     catch(err)
     {
+        console.log("rune error " + err.message);
         if(err.message.includes("ClientModeError"))
         {
             await initRun(width, height, false);
@@ -107,9 +104,11 @@ async function initRun(width, height, client)
 async function loadAvatar(runInstance, avatarLoc)
 {
     runInstance.activate();
+    console.log("about to load avatar");
     let ava = await runInstance.load(avatarLoc);
     await ava.sync({inner: false});
-    root.localStorage.sabroshiAvatar = ava.location;  //updating local storage to last known avatar location for faster sync times
+    console.log("avaloaded: " + ava.location);
+    window.localStorage.sabroshiAvatar = ava.location;  //updating local storage to last known avatar location for faster sync times
     return ava;
 }
 
